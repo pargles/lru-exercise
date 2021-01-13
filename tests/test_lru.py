2d5d94087e48ea08ca8a4d74d23810f80a375a12
Pargles Dall'Oglio <pargles@rossintelligence.com>
2021-01-13 08:46:48 -0500

    add tests for lru

---
+++ tests/test_lru.py
@@ -0,0 +1,29 @@
+from src.LRUCache import LRUCache
+
+def test_small_capacity():
+    expected_length = 2
+    lru = LRUCache(expected_length)
+    lru.put("1", "bananas")
+    lru.put("2", "pineapple")
+    lru.put("3", "kiwi")
+    assert len(lru.cache) == expected_length
+    assert lru.array.get_length() == expected_length
+
+
+def test_delete():
+    lru = LRUCache(3)
+    lru.put("1", "bananas")
+    lru.delete("1")
+    assert len(lru.cache) == 0
+    assert lru.array.get_length() == 0
+
+
+def test_invert_positions_on_get():
+    lru = LRUCache(2)
+    lru.put("1", "bananas")
+    lru.put("2", "blueberries")
+    lru.get("1")
+    assert len(lru.cache) == 2
+    assert lru.array.get_length() == 2
+    assert lru.array.head.key == "2"
+
